a89a4c2dfa0c7e66f75918b8d089732d690dc257
victorpuente <v.puente.castro@gmail.com>
2019-09-25 14:30:41 +0200

    mas intentos

--- src/app/favoritesForkLift/favoritesForkLift.page.ts
+++ src/app/favoritesForkLift/favoritesForkLift.page.ts
@@ -5,8 +5,6 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Product } from '../models/products'
 import { MenuController } from '@ionic/angular';
 
-
-
 @Component({
   selector: 'app-favoritesForkLift',
   templateUrl: './favoritesForkLift.page.html',
@@ -40,13 +38,6 @@ export class FavoritesForkLiftPage implements OnInit {
     }
   }
 
-  // setFilteredItems(){
-
-  //   for(let i=0; i>this.products.length; i++){
-  //     if(this.products[i].title )
-  //   }
-  // }
-
   async getData(){
     const loading = await this.loadingCtrl.create({
       message: 'Please wait...'
@@ -75,3 +66,99 @@ export class FavoritesForkLiftPage implements OnInit {
   }
 
 }
+
+
+
+// @Component({
+//   selector: 'app-favoritesForkLift',
+//   templateUrl: './favoritesForkLift.page.html',
+//   styleUrls: ['./favoritesForkLift.page.scss'],
+// })
+// export class FavoritesForkLiftPage{
+
+//     favorites: Array<any>;
+
+  
+//     constructor(public navCtrl: NavController, public service: PropertyService) {
+//       this.getFavorites();
+//   }
+
+//   itemTapped(favorite) {
+//       this.navCtrl.push(PropertyDetailPage, favorite.property);
+//   }
+
+//   deleteItem(favorite) {
+//       this.service.unfavorite(favorite)
+//           .then(() => {
+//               this.getFavorites();
+//           })
+//           .catch(error => alert(JSON.stringify(error)));
+//   }
+
+//   getFavorites() {
+//       this.service.getFavorites()
+//           .then(data => this.favorites = data);
+//   }
+
+// }
+  
+//   products: Product[];
+//   searchTerm: string = "";
+//   filteredItems: Product[];
+//   knobValues: any = {
+//     upper:0,
+//     lower:200
+//   }
+ 
+
+
+//   constructor(
+//     public loadingCtrl: LoadingController,
+//     private authService: AuthService,
+//     private router: Router,
+//     private route: ActivatedRoute,
+//     private menuCtrl: MenuController,
+    
+//   ) { }
+
+//   ngOnInit() {
+//     if (this.route && this.route.data) {
+//       this.getData();
+//     }
+//   }
+
+//   // setFilteredItems(){
+
+//   //   for(let i=0; i>this.products.length; i++){
+//   //     if(this.products[i].title )
+//   //   }
+//   // }
+
+//   async getData(){
+//     const loading = await this.loadingCtrl.create({
+//       message: 'Please wait...'
+//     });
+//     this.presentLoading(loading);
+
+//     this.route.data.subscribe(routeData => {
+//       routeData['data'].subscribe(data => {
+//         loading.dismiss();
+//         this.products = data;
+//       })
+//     })
+//   }
+
+//   async presentLoading(loading) {
+//     return await loading.present();
+//   }
+
+//   logout(){
+//     this.authService.doLogout()
+//     .then(res => {
+//       this.router.navigate(["/login"]);
+//     }, err => {
+//       console.log(err);
+//     })
+//   }
+
+// }
